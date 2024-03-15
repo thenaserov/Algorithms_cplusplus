754e99b2d130e0f9af155781beedb4dc9ea9dd57
Mahdi Movahedi <m.movahedimonfared@gmail.com>
2024-03-15 23:39:26 +0330

    changes made: changing type of i and j adding copy algorithm to the code instead of using for loop and insertion operator to ostream adding comments about thchanges where ever there was one
    Signed-off-by: Mahdi Movahedi <m.movahedimonfared@gmail.com>

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -1,25 +1,42 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <iterator>
 
-std::vector<int> InsertionSort(std::vector<int> input_vector) { 
-	int key = 0, i = 0;
-	for(int j = 2; j < input_vector.size(); ++j) {
+template <typename RandAccessContainer>
+RandAccessContainer &InsertionSort(RandAccessContainer &input_vector)
+{
+	int key = 0;
+	for (size_t j = 2; j < input_vector.size(); ++j)
+	{
 		key = input_vector[j];
+		// I changed the location of i declaration and its type, so as j, to eliminate not-safe conversions between i and j 
+		// to signed types 
+		size_t i = 0;
 		i = j - 1;
-		while (i > 0 && input_vector[i] > key) {
+		while (i > 0 && input_vector[i] > key)
+		{
 			input_vector[i + 1] = input_vector[i];
-			i = i -1;
+			i = i - 1;
 		}
 		input_vector[i + 1] = key;
 	}
 	return input_vector;
 }
 
-int main() {
+int main()
+{
 	std::vector<int> unsorted = {1, 9, 8, 2, 3, 5, 7, 6, 4};
-	std::vector<int> sorted   = InsertionSort(unsorted);
-	for(std::vector<int>::iterator iter = sorted.begin();iter < sorted.end(); iter ++) { 
-		std::cout << *iter << std::endl;
-	}
+	auto sorted = InsertionSort<>(unsorted);
+
+	// for(std::vector<int>::iterator iter = sorted.begin();iter < sorted.end(); iter ++) {
+	// 	std::cout << *iter << std::endl;
+	// }
+
+	// The repo name addresses algorithm, I thought it would be nice if we made use of algorithms as much as possible
+	// inorder to make it independent of the type on which unsorted and sorted has been intialized I used decltype,
+	// although it is that much neat!
+	std::copy(std::begin(sorted), std::end(sorted), std::ostream_iterator<decltype(*sorted.begin())>{std::cout, ", "});
+	std::cout << std::endl;
 	return 0;
 }
