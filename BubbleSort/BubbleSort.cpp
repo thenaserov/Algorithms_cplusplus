7439dbeabf30a548a733f9a281f68bbd4452110d
Naser Rezayi <54706692+thenaserov@users.noreply.github.com>
2024-12-15 21:04:47 +0330

    Update BubbleSort.cpp

--- BubbleSort/BubbleSort.cpp
+++ BubbleSort/BubbleSort.cpp
@@ -1,13 +1,27 @@
+// C++ program for the implementation of Bubble sort
+/*Start by comparing the first two elements.If they are in the wrong order, swap them.
+Continue this process for all elements moving from left to right.After the first pass, the largest element will be at the end.
+In the next pass, skip the last element since it’s already sortedand repeat the above steps.The second - largest element will move to the second - last position.
+Repeat the steps until the entire array is sorted.*/
+
 #include <iostream>
 #include <vector>
+using namespace std;
 
-BubbleSort(std::vector<int> unsorted_vector)
-{
-    std::vector<int> sorted;
+void bubbleSort(vector<int>& v) {
+    int n = v.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (v[j] > v[j + 1])
+                swap(v[j], v[j + 1]);
+        }
+    }
 }
 
-int main()
-{
-
+int main() {
+    vector<int> v = { 5, 1, 4, 2, 8 };
+    bubbleSort(v);
+    for (auto i : v)
+        cout << i << " ";
     return 0;
 }
